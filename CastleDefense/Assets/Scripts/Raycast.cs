c2668708a2401d07537a4d3a0c678f6b451fecd7
L2dulgi <2000daehee@gmail.com>
2019-11-15 14:05:53 +0900

    raycast added

---
+++ CastleDefense/Assets/Scripts/Raycast.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public GameObject intersect_point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update (){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        // if(Physics.Raycast(ray,out hit))
+        // {
+        //     // Debug.Log(hit.transform.name);
+        //     Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+        //     Debug.Log("Did Hit");
+        // }
+        if (Physics.Raycast(ray, out hit, 100)){
+            Debug.DrawLine(ray.origin, hit.point);
+            Debug.Log(hit.point);
+            if(hit.transform.name == "Ground"){
+                // intersect_point.transform.Translate(new Vector3(hit.point.x,0.0f,hit.point.z));
+                intersect_point.transform.position = new Vector3(hit.point.x,0.0f,hit.point.z);
+            }
+        }
+	}
+}
--- CastleDefense/Assets/Scripts/TestPlayer.cs
+++ CastleDefense/Assets/Scripts/TestPlayer.cs
@@ -7,12 +7,25 @@ public class TestPlayer : MonoBehaviour
 {
     // 마법 prefab 받아오기
     public GameObject magic;
+    public GameObject intersect_point;
+
     void Update()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100)){
+            Debug.DrawLine(ray.origin, hit.point);
+            Debug.Log(hit.point);
+            if(hit.transform.name == "Ground"){
+                intersect_point.transform.position = new Vector3(hit.point.x,0.0f,hit.point.z);
+            }
+        }
         // 마우스 왼클릭하면 마법 발동
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)){
             // Instantiate(마법 prefab, 마법 prefab 소환할 좌표, 마법 prefab의 회전값 설정)
             // 이건 해일을 위한 Instantiate
-            Instantiate(magic, new Vector3(0, 4.5f, -40f), Quaternion.identity);
+            
+            Instantiate(magic, intersect_point.transform.position, Quaternion.identity);
+        }
     }
 }
